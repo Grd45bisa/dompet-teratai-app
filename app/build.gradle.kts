0c029a824d2add8b71b760ec8aa7d711d6e3810f
Ubuntu <ubuntu@ip-172-31-9-159.ap-southeast-3.compute.internal>
2026-02-07 15:06:41 +0000

    fix: add jitpack repo + appcompat for cropper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
